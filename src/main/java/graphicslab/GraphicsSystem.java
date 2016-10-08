553f8b0d552194fabc66f7ce9c63bbb44085737e
Trevor Thai Kim Nguyen <trevortknguyen@gmail.com>
2016-10-08 12:32:40 -0500

    Align GraphicsSystem with GLFW behavior

--- src/main/java/graphicslab/GraphicsSystem.java
+++ src/main/java/graphicslab/GraphicsSystem.java
@@ -4,35 +4,62 @@ import static org.lwjgl.glfw.GLFW.*;
 
 import org.lwjgl.glfw.GLFWErrorCallback;
 
+/**
+ * Wrapper for LWJGL's
+ * <a href="https://javadoc.lwjgl.org/org/lwjgl/glfw/GLFW.html">GLFW</a> native
+ * Java bindings which handles windowing.
+ * 
+ * Handles the necessary static calls in order initialize and terminate the GLFW system.
+ * 
+ * @author trevortknguyen
+ */
 public class GraphicsSystem {
 	private static boolean isActive = false;
 
 	private static GLFWErrorCallback errorCallback;
 
-	public static void init() {
+	/**
+	 * Creates and sets the GLFW error callback to {@link System.err}.
+	 * Initializes the GLFW library.
+	 * 
+	 * <b>This is required before the graphicslab windowing system is able to be
+	 * used.</b>
+	 *
+	 * Returns true if successful or throws {@link IllegalStateException} in
+	 * case GLFW failed to initialize.
+	 */
+	public static boolean init() {
 		if (isActive()) {
-			throw new UnsupportedOperationException("Can not initialize active graphics system.");
+			return true;
 		}
-		
+
 		errorCallback = GLFWErrorCallback.createPrint(System.err);
 		errorCallback.set();
 
 		if (!(isActive = glfwInit())) {
 			throw new IllegalStateException("Unable to initialize GLFW.");
 		}
+
+		return true;
 	}
 
+	/**
+	 * Destroys any remaining windows or cursors. Frees the error callback. Once
+	 * this function is called, you must call init() again to use any of the
+	 * windowing functions.
+	 */
 	public static void terminate() {
-		if (!isActive()) {
-			throw new UnsupportedOperationException("Can not terminate inactive graphics system.");
-		}
-		
 		glfwTerminate();
 
 		GLFWErrorCallback previousCallback = glfwSetErrorCallback(null);
-		previousCallback.free();
+		if (previousCallback != null) {
+			previousCallback.free();
+		}
 	}
 
+	/**
+	 * @return if graphics system is currently initialized.
+	 */
 	public static boolean isActive() {
 		return isActive;
 	}
--- src/test/java/graphicslab/GraphicsSystem_Test.java
+++ src/test/java/graphicslab/GraphicsSystem_Test.java
@@ -1,29 +1,24 @@
 package graphicslab;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import static org.junit.Assert.*;
 
 public class GraphicsSystem_Test {
-	@Rule
-	public ExpectedException exception = ExpectedException.none();
 	
 	@Test
 	public void multipleInitCalls() {
-		exception.expect(UnsupportedOperationException.class);
-		GraphicsSystem.init();
-		GraphicsSystem.init();
+		assertTrue(GraphicsSystem.init());
+		assertTrue(GraphicsSystem.init());
 	}
 	
 	@Test
 	public void initAndTerminate() {
-		GraphicsSystem.init();
+		assertTrue(GraphicsSystem.init());
 		GraphicsSystem.terminate();
 	}
 	
 	@Test
-	public void terminateWithoutInit() {
-		exception.expect(UnsupportedOperationException.class);
-		GraphicsSystem.terminate();
+	public void verifyDefaultInactive() {
+		assertFalse(GraphicsSystem.isActive());
 	}
 }
